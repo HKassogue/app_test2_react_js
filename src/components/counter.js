dcdda7660a0f610b1c125fd5b39aa460ff7d0ee1
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-04 19:27:22 +0000

    add of simple page counter

---
+++ src/components/counter.js
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div className="card">
+                <div className="card-header">Title</div>
+                <div className="card-body">
+                    Content
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Counter;
