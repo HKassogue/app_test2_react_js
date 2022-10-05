c86809a4605ca7d37fae3786d901e3d61ad65833
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-05 15:18:50 +0000

    use of Routes system

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import Counter from './components/counter';
+import About from './components/about';
+import {Route, Routes, Link, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
-    <div className='m-3'>
-      <Counter title="Profile" value={1} image="images/profile.png"/>
-      <Counter title="User" value={1} image="images/user.jpg"/>
-    </div>
+    // <div className='m-3'>
+    //   <Counter title="Profile" value={1} image="images/profile.png"/>
+    //   <Counter title="User" value={1} image="images/user.jpg"/>
+    // </div>
+
+    <Router>
+      <nav className='navbar navbar-expand navbar-brand m-2'>
+        <ul className='navbar-nav'>
+          <li>
+            <Link className='nav-link' to="/home">Home</Link>
+          </li>
+          <li>
+            <Link className='nav-link' to="/counter">Counter</Link>
+          </li>
+          <li>
+            <Link className='nav-link' to="/about">About</Link>
+          </li>
+          <li>
+            <Link className='nav-link' to="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className='container'>
+        <Routes>
+          <Route path="/home"> </Route>
+          <Route path="/counter" element={<Counter/>}> </Route>
+          <Route path="/about" element={<About/>}> </Route>
+          <Route path="/gallery"> </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
