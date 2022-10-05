f5865f52ddbcec3d7a6114247909eec04a4f7eef
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-05 14:32:07 +0000

    use of setState on conter page with +- buttons

--- src/components/counter.js
+++ src/components/counter.js
@@ -4,6 +4,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Counter extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            counter: 1,
+            list: [0]
+        }
+    }
+
+    compute = (op) => {
+        let c = this.state.counter + 1;
+        this.setState({
+            counter: c,
+            list: new Array(c).fill(0)
+        });
     }
 
     render() {
@@ -11,11 +23,19 @@ class Counter extends Component {
             <div className="card m-3">
                 <div className="card-header">
                     <strong>
-                        {this.props.title} : {this.props.value}
+                        {this.props.title} : {this.state.counter}
                     </strong>
                 </div>
+                <div className="align-self-end">
+                    <button onClick={()=>this.compute('+')} className="btn btn-primary m-2">+</button>
+                    <button onClick={()=>this.compute('+')} className="btn btn-primary m-2">-</button>
+                </div>
                 <div className="card-body">
-                    <img width={100} src={this.props.image}/>
+                    {
+                        this.state.list.map((value,index) => 
+                            <img width={100} src={this.props.image}/>
+                        )
+                    }
                 </div>
             </div>
         );
