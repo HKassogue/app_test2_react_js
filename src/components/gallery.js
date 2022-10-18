080fa6e404d12ae81fd07f722dbabe4c21dc5fea
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-18 16:39:08 +0000

    add of research form with keyword

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -28,23 +28,46 @@ class Gallery extends Component {
         }))
     }
 
+    setKeyword = (event) => {
+        this.setState({
+            currentKeyword: event.target.value
+        });
+    }
+
     render() {
         return (
-            <div className="row">
-                {
-                    this.state.hits.map(hit =>
-                        <div className="col-md-4">
-                            <div className="card">
-                                <div className="card-header">
-                                    {hit.tags} | {hit.webformatWidth} x {hit.webformatHeight}
-                                </div>
-                                <div className="card-body">
-                                    <img className="card-img"  height={200} src={hit.webformatURL} />
+            <div>
+                <form>
+                    <div className="row m-1 p-1">
+                        {//<div>{this.state.currentKeyword}</div>
+                        }
+                        <div className="col">
+                            <input type="text" 
+                                value={this.state.currentKeyword}
+                                onChange={this.setKeyword}
+                                className="form-control" />
+                        </div>
+                        <div className="col-auto">
+                            <button className="btn btn-success">Chercher</button>
+                        </div>
+                    </div>
+                </form>
+                <div className="row">
+                    {
+                        this.state.hits.map(hit =>
+                            <div className="col-md-4">
+                                <div className="card">
+                                    <div className="card-header">
+                                        {hit.tags} | {hit.webformatWidth} x {hit.webformatHeight}
+                                    </div>
+                                    <div className="card-body">
+                                        <img className="card-img"  height={200} src={hit.webformatURL} />
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    )
-                }
+                        )
+                    }
+                </div>
             </div>
         );
     }
