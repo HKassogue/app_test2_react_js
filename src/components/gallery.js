3dfc18b071cba99e0e0bb77a03771c456ef2a5a5
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-18 15:58:40 +0000

    installing axios & getting hits from api

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,6 +1,33 @@
 import React, {Component} from "react";
+import axios from "axios";
 
 class Gallery extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hits: [],
+            currentPage: 1,
+            pageSize: 10,
+            currentKeyword: 'paris'
+        }
+    }
+
+    componentDidMount() {
+        this.getHits();
+    }
+
+    getHits() {
+        let url = "https://pixabay.com/api/?key=5832566-81dc7429a63c86e3b707d0429&q=paris";
+        axios.get(url).then((resp) => {
+            console.log(resp);
+            this.setState({
+                hits: resp.data.hits
+            });
+        }).catch((err => {
+            console.log(err);
+        }))
+    }
+
     render() {
         return (
             <div>
