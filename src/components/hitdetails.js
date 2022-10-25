4d7a9f13084390ebdf9c068cb0994771891b7835
Hamidou KASSOGUE <hamidoukass@gmail.com>
2022-10-25 11:40:37 +0000

    getting item in hitDetails and displaying (dont work)

--- src/components/hitdetails.js
+++ src/components/hitdetails.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react';
+import axios from "axios";
+import HitItem from './hititem';
 
 class HitDetails extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            hit: null,
+        }
+    }
+
+    getHit(id) {
+        let url = "https://pixabay.com/api/?key=5832566-81dc7429a63c86e3b707d0429&id=" + id;
+        axios.get(url).then((resp) => {
+            this.setState({
+                hit: resp.data.hits[0],
+            });
+        }).catch((err => {
+            console.log(err);
+        }))
+    }
+
+    componentDidMount() {
+        //this.getHit(this.props.match.params.id);
+    }
+
     render() {
-        return (
-            <div>
-                <h2>Hit details</h2>
-            </div>
-        );
+        if(this.state.hit != null) 
+            return (
+                <HitItem hit={this.state.hit} />             
+            );
+        else
+            return (
+                <div>
+                    
+                </div>
+            ); 
     }
 }
 
